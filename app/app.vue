c2839f2fd89ade77c7ce7a85c24b6a5d5ad969cf
NamesMT <dangquoctrung123@gmail.com>
2024-05-26 17:22:16 +0000

    chore: init

---
+++ app/app.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const { data } = await useLazyAsyncData('apiResult', () => new Promise(r => setTimeout(r, 0)).then(() => `Hello! - ${Date.now()}`), { default: () => 'Loading...' as const })
+</script>
+
+<template>
+  <div>
+    This should be SSG and not change: <span :style="{ color: 'red' }">{{ data }}</span>
+  </div>
+</template>
